2f6cfcf9ce4a8062e4f8ba4df61bb15f71e1ec0d
Abdulrahman Alhamodi <aalha028@uottawa.ca>
2017-01-28 23:06:42 -0500

    Add mage character

---
+++ Source/InfiniEx/Public/Animations/InfiniMageAnimation.h
@@ -0,0 +1,19 @@
+// Copyright Abdulrahman Alhamoudi, 2016 - All rights reserved.
+
+#pragma once
+
+#include "Animations/InfiniAnimation.h"
+#include "InfiniMageAnimation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFINIEX_API UInfiniMageAnimation : public UInfiniAnimation
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
